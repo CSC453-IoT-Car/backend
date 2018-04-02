a32a78102ea3351eaa735dd8ebbd359f59408628
Jack Schaffner <jackwschaffner@gmail.com>
2018-04-02 00:36:06 -0400

    Runnable.

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Server Online')
+})
+
+app.listen(8000, function () {
+  console.log('Backend started on port 8000.')
+})
