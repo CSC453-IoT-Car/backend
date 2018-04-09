04cf41ddf3255047c95b51b9d1978b23518c4c9e
Jack Schaffner <jackwschaffner@gmail.com>
2018-04-08 21:30:37 -0400

    Working two way communication.

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var request = require('request');
+
+var nextId = 0;
 
 app.get('/', function (req, res) {
-  res.send('Server Online')
-})
+    res.send('Server Online');
+});
+
+app.post('/heartbeat', function (req, res) {
+    res.send('Received heartbeat.');
+});
+
+app.post('/register', function (req, res) {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('Registered vehicle with ip ' + ip.split(':')[3]);
+    res.json({id: nextId});
+    nextId++;
+    request({
+        url: 'http://' + ip.split(':')[3] + ':8000/move',
+        method: "POST",
+        json: {}
+    }, function (err, res, body) {
+        if (res.statusCode != 200) {
+            console.log(res.toJSON());
+        } else {
+            console.log('Sent move command.');
+        }
+    });
+});
 
 app.listen(3000, function () {
-  console.log('Backend started on port 3000.')
-})
+    console.log('Backend started on port 3000.')
+});
